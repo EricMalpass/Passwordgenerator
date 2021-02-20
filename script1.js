fe5787a3dbf235c149b4dfe78e7bd5d058a5ffdb
EricMalpass <ericmalpass@gmail.com>
2021-02-19 18:28:41 -0800

    update

--- script1.js
+++ script1.js
@@ -1,52 +1,52 @@
 
-var includepasswordLengthElement = document.getElementById ('includepasswordLength')
-var includeuppercaseElement = document.getElementById ('includeuppercase')
-var includenumbersElement= document.getElementById ('includenumbers')
-var includeSpecialCharactersElement = document.getElementById ('includeSpecialCharacters')
-var includelowercaseElement = document.getElementById ('includelowercase')
+var includePasswordLengthElement=document.getElementById ('includePasswordLength')
+var includeUppercaseElement=document.getElementById ('includeUppercase')
+var includeNumbersElement=document.getElementById ('includeNumbers')
+var includeSpecialCharactersElement=document.getElementById ('includeSpecialCharacters')
+var includeLowercaseElement=document.getElementById ('includeLowercase')
 
-var passwordLength = document.getElementById ('passwordlength')
-var uppercase_Code = arrayFromHighToLow (65, 90)
-var lowercase_Code = arrayFromHighToLow (97, 122)
-var numbers_Code = arrayFromHighToLow (48, 57)
-var specialCharacter_Code = arrayFromHighToLow (33, 47).concat(arrayFromHighToLow (58,64)
-).concat(arrayFromHighToLow(91, 96)
-).concat(arrayFromHighToLow(123, 126))
+var passwordLength=document.getElementById ('passwordLength')
+var uppercaseCode=arrFromHighToLow (65, 90)
+var lowercaseCode=arrFromHighToLow (97, 122)
+var numbersCode=arrFromHighToLow (48, 57)
+var specialCharacterCode=arrFromHighToLow (33, 47).concat(arrFromHighToLow (58,64)
+).concat(arrFromHighToLow(91, 96)
+).concat(arrFromHighToLow(123, 126))
 
-var btn = document.getElementById('generate')
-var passwordDisplay = document.getElementById('passwordDisplay')
+var btn=document.getElementById('generate')
+var passwordDisplay=document.getElementById('passwordDisplay')
 
 btn.addEventListener('click', function(e){
     e.preventDefault()
-    var includepasswordLength = passwordLength.value
-    var includeuppercase = includeuppercaseElement.checked
-    var includenumbers = includenumbersElement.checked
-    var includeSpecialCharacters = includeSpecialCharactersElement.checked
-    var password = generatePassword (passwordLength.value, includeuppercaseElement.checked, includenumbersElement.checked, includeSpecialCharactersElement.checked)
-    passwordDisplay.innerText = password
+    var includePasswordLength=passwordLength.value
+    var includeUppercase=includeUppercaseElement.checked
+    var includeNumbers=includeNumbersElement.checked
+    var includeSpecialCharacters=includeSpecialCharactersElement.checked
+    var password=generatePassword (passwordLength.value, includeUppercaseElement.checked, includeNumbersElement.checked, includeSpecialCharactersElement.checked)
+    passwordDisplay.innerText=password
 })
 
-function generatePassword(passwordLength, includenumbers, includeuppercase, includespecialCharacters) {
-    let charCodes = lowercase_Code
-    console.log ('hello')
-    if (includeuppercase) charCodes = charCodes.concat(uppercase_Code)
+function generatePassword(passwordLength, includeNumbers, includeUppercase, includeSpecialCharacters) {
+    let charCodes=lowercase_Code
+    console.log (charCodes)
+    if (includeUppercase) charCodes=charCodes.concat(uppercaseCode)
     console.log ('hello2')
-    if (includenumbers) charCodes = charCodes.concat(numbers_Code)
+    if (includeNumbers) charCodes=charCodes.concat(numbersCode)
     console.log ('hello3')
-    if (includespecialCharacters) charCodes = charCodes.concat(specialCharacter_Code)
+    if (includeSpecialCharacters) charCodes=charCodes.concat(specialCharacterCode)
     console.log ('hello4')
     
     var passwordCharacters = []
     for (let i = 0; i < passwordLength; i++) {
-        var character = charCodes[Math.floor(Math.random() * passwordLength)]
+        var character=charCodes[Math.floor(Math.random() * passwordLength)]
         passwordCharacters.push (String.fromCharCode(character))
     }
     return passwordCharacters.join('')
 }
-function arrayFromHighToLow (low, high){
-    var array =[]
-    for (let i = low; i<= high; i++){
+function arrFromHighToLow (low, high){
+    var arr=[]
+    for (let i=low; i<=high; i++){
         array.push(i)
     }
-    return array
+    return arr
 }
