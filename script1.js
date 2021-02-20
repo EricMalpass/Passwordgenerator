01ab9c73f657d9955f7df005ebc1818542ff4fee
EricMalpass <ericmalpass@gmail.com>
2021-02-19 16:17:09 -0800

    again

--- script1.js
+++ script1.js
@@ -1,10 +1,11 @@
 
 var includepasswordLengthElement = document.getElementById ('includepasswordLength')
 var includeuppercaseElement = document.getElementById ('includeuppercase')
-var includelowercaseElement = document.getElementById ('includenumbers')
-var includespecialCharactersElement = document.getElementById ('includespecialCharacters')
+var includenumbersElement= document.getElementById ('includenumbers')
+var includeSpecialCharactersElement = document.getElementById ('includeSpecialCharacters')
+var includelowercaseElement = document.getElementById ('includelowercase')
 
-
+var passwordLength = document.getElementById ('passwordlength')
 var uppercase_Code = arrayFromHighToLow (65, 90)
 var lowercase_Code = arrayFromHighToLow (97, 122)
 var numbers_Code = arrayFromHighToLow (48, 57)
@@ -12,27 +13,28 @@ var specialCharacter_Code = arrayFromHighToLow (33, 47).concat(arrayFromHighToLo
 ).concat(arrayFromHighToLow(91, 96)
 ).concat(arrayFromHighToLow(123, 126))
 
-var form = document.getElementById (passwordGeneratorForm)
-var passwordDisplay = document.getElementById ('passwordDisplay')
+var btn = document.getElementById('generate')
+var passwordDisplay = document.getElementById('passwordDisplay')
 
-form.addEventListener ('generate', e => {
+btn.addEventListener('click', function(e){
     e.preventDefault()
     var includepasswordLength = passwordLength.value
     var includeuppercase = includeuppercaseElement.checked
-    var includelowercase = 
     var includenumbers = includenumbersElement.checked
     var includeSpecialCharacters = includeSpecialCharactersElement.checked
-    var password = generatePassword (passwordLength, includeUppercase, includeLowercase, IncludeNumbers, ImcludeSpecialCharacters)
+    var password = generatePassword (passwordLength.value, includeuppercaseElement.checked, includenumbersElement.checked, includeSpecialCharactersElement.checked)
     passwordDisplay.innerText = password
 })
 
-
-
-function generatePassword (passwordLength, includenumbers, includeuppercase, includelowercase, includespecialCharacters) {
+function generatePassword(passwordLength, includenumbers, includeuppercase, includespecialCharacters) {
     let charCodes = lowercase_Code
+    console.log ('hello')
     if (includeuppercase) charCodes = charCodes.concat(uppercase_Code)
+    console.log ('hello2')
     if (includenumbers) charCodes = charCodes.concat(numbers_Code)
+    console.log ('hello3')
     if (includespecialCharacters) charCodes = charCodes.concat(specialCharacter_Code)
+    console.log ('hello4')
     
     var passwordCharacters = []
     for (let i = 0; i < passwordLength; i++) {
